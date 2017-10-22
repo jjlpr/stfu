e11871004ed09b3c370b67a2ecf2acbb5a4beff4
James Cooke <jamescooke@users.noreply.github.com>
2017-10-22 17:06:31 +0100

    Lengthen sound file to 60 seconds and disable software volume

--- capture.sh
+++ capture.sh
@@ -5,5 +5,5 @@ set -eo pipefail
 now=$(date +%s%3N)
 wav_filename="soundfiles/$now.wav"
 
-arecord --device=hw:1,0 -f S16_LE -d 1 "$wav_filename"
+arecord --disable-softvol --device=hw:1,0 -f S16_LE -d 60 "$wav_filename"
 venv/bin/python -m stfu "$wav_filename" >> output.json
