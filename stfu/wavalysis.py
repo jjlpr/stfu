a6c46d4f7ec2889455957c8b4936b1aa0763b6e2
James Cooke <jamescooke@users.noreply.github.com>
2017-10-22 13:23:00 +0100

    Analyse wav file

--- stfu/wavalysis.py
+++ stfu/wavalysis.py
@@ -1,2 +1,18 @@
-def wav_to_json(wav_file):
-    return ''
+import numpy
+from scipy.io import wavfile
+
+
+def wav_to_stats(wav_file):
+    """
+    Args:
+        wav_file (str): fully qualified location of the wav file to be
+            analysed.
+
+    Returns:
+        tuple (int, int): Average and max absolute volumes returned from the
+            wav file.
+    """
+    data = wavfile.read(wav_file)[1]
+    data = numpy.absolute(data)
+
+    return int(numpy.round(numpy.average(data))), data.max()
--- tests/test_wavalysis.py
+++ tests/test_wavalysis.py
@@ -1,7 +1,14 @@
-from stfu.wavalysis import wav_to_json
+from stfu.wavalysis import wav_to_stats
+
+import os
 
 
-def test():
-    result = wav_to_json('')
+def test_quiet():
+    """
+    1508673476734 is a quiet file recorded off the audio card with no input
+    """
+    quiet_file = os.path.join(os.path.dirname(__file__), 'data', '1508673476734.wav')
 
-    assert result == ''
+    result = wav_to_stats(quiet_file)
+
+    assert result == (2, 10)
